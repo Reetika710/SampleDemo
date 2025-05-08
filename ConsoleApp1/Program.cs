e6c8537df7a45dba1fd5ba3bfb9e6c88674b77b4
Reetika <rnarula@atlassian.com>
2025-05-08 17:42:52 +0530

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number or a string");
+            string s = Console.ReadLine();
+            Program p = new Program();
+            string t = p.Reverse(s);
+            Console.WriteLine("The reverse of {0} is {1}", s, t);
+
+        }
+
+        public string Reverse(string str)
+        {
+            char[] cArray = str.ToCharArray();
+            string reverse = "";
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+
+        }
+    }
+}
